a0268f6ab1206b28a70e23492d82c2c527b5a236
Kimiko Kano <kskano@gmail.com>
2016-05-25 19:43:58 -0600

    Prevent tile placement on other tiles
    Remove event listeners after tile is placed.

--- test/tile-inventory-test.js
+++ test/tile-inventory-test.js
@@ -20,16 +20,16 @@ describe('TileInventory', () => {
     });
   });
 
-  describe('findAdjacentCoordinates', () => {
-    it('adds x and y coordinates of position around tile to validPositions array', () => {
+  describe('addAdjacentCoordinates', () => {
+    it('adds x and y coordinates of position around tile to adjacentPositions array', () => {
       let tileInventory = new TileInventory();
       let tile1 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 100, 100);
       let tile2 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 200, 200);
       let tile3 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 300, 300);
 
-      tileInventory.findAdjacentCoordinates(500, 500);
+      tileInventory.addAdjacentCoordinates(500, 500);
       let expected = [ [ 500, 400 ], [ 600, 500 ], [ 500, 600 ], [ 400, 500 ] ]
-      assert.deepEqual(tileInventory.validPositions, expected, "finds valid coordinates");
+      assert.deepEqual(tileInventory.adjacentPositions, expected, "finds adjacent coordinates");
     });
   });
 });
