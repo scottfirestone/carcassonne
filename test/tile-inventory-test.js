24d5f5ee623bf090ff8463656da6b32e11e5113a
Kimiko Kano <kskano@gmail.com>
2016-05-25 19:07:10 -0600

    Tiles can only be placed next to other tiles

--- test/tile-inventory-test.js
+++ test/tile-inventory-test.js
@@ -19,4 +19,17 @@ describe('TileInventory', () => {
       assert.isFalse(tileInventory.isPositionOpen(100, 100));
     });
   });
+
+  describe('findAdjacentCoordinates', () => {
+    it('adds x and y coordinates of position around tile to validPositions array', () => {
+      let tileInventory = new TileInventory();
+      let tile1 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 100, 100);
+      let tile2 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 200, 200);
+      let tile3 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 300, 300);
+
+      tileInventory.findAdjacentCoordinates(500, 500);
+      let expected = [ [ 500, 400 ], [ 600, 500 ], [ 500, 600 ], [ 400, 500 ] ]
+      assert.deepEqual(tileInventory.validPositions, expected, "finds valid coordinates");
+    });
+  });
 });
